db0d0581422756af23717bf32ac27682bad16b39
Nirvana <nirvana.vella.g53246@mcast.edu.mt>
2025-04-25 23:52:18 +0200

    Timer added into the game

--- SoundMemoryMatchProto/Assets/Scripts/RandomNoteGenerator.cs
+++ SoundMemoryMatchProto/Assets/Scripts/RandomNoteGenerator.cs
@@ -6,15 +6,23 @@ public class RandomNoteGenerator : MonoBehaviour
     //empty game object will be needed in the unity editor to allow the note to generate
     public GameObject randomNoteGenerator;
     //list of array with music notes
-    string[] notes = { "A", "B", "C", "D", "E", "F", "G" };
+    //string[] notes = { "A", "B", "C", "D", "E", "F", "G" };
     public GameObject[] notesPrefabs;
+
+    //transform variable to get position of the actual spawn point which in this case is the spawning of note variable
+    public Transform spawnPoint;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //kept showing ambuguity error without unityengine
-        int range = UnityEngine.Random.Range(1, 8);
+        int range = UnityEngine.Random.Range(1, notesPrefabs.Length);
+        //gameobject for spawn point
+        GameObject spawningOfNote = notesPrefabs[range];
         Debug.Log(range);
+
+        //spawning the actual notes
+        Instantiate(spawningOfNote, spawnPoint.position, Quaternion.identity);
     }
 
     // Update is called once per frame
---
+++ SoundMemoryMatchProto/Assets/Scripts/Timer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    float timeLeft = 90f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timerText.text = timeLeft.ToString();
+    }
+}
