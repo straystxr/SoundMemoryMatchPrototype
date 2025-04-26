fa5fc607b2baa322294971044b17b3d6bf20c9a8
Nirvana <nirvana.vella.g53246@mcast.edu.mt>
2025-04-26 17:28:16 +0200

    Stopping game + Looping sequence of notes spawning
    + Once the timer reaches zero the notes will stop spawning
    + Notes spawned every second and also destroying the asset to avoid confusion

--- SoundMemoryMatchProto/Assets/Scripts/RandomNoteGenerator.cs
+++ SoundMemoryMatchProto/Assets/Scripts/RandomNoteGenerator.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class RandomNoteGenerator : MonoBehaviour
@@ -12,22 +14,52 @@ public class RandomNoteGenerator : MonoBehaviour
     //transform variable to get position of the actual spawn point which in this case is the spawning of note variable
     public Transform spawnPoint;
 
+    //variables for timer 
+    [SerializeField] TextMeshProUGUI timerText;
+    float timeLeft = 90f;
+    bool timerStatus = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //kept showing ambuguity error without unityengine
-        int range = UnityEngine.Random.Range(0, notesPrefabs.Length);
-        //gameobject for spawn point
-        GameObject spawningOfNote = notesPrefabs[range];
-        Debug.Log(range);
-
-        //spawning the actual notes
-        Instantiate(spawningOfNote, spawnPoint.position, Quaternion.identity);
+        StartCoroutine(spawnNotes());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timerStatus)
+        {
+            timeLeft -= Time.deltaTime;
+            //will not allow the timer to go below 0 w the mathf.max
+            timeLeft = Mathf.Max(0f, timeLeft);
+            timerText.text = timeLeft.ToString();
+        }
+
+    }
+
+    IEnumerator spawnNotes()
+    {
+        while (timerStatus) {
+            if (timeLeft > 0)
+            {
+                //kept showing ambuguity error without unityengine
+                int range = UnityEngine.Random.Range(0, notesPrefabs.Length);
+                //gameobject for spawn point
+                //spawning the actual notes
+                GameObject spawningOfNote = Instantiate(notesPrefabs[range], spawnPoint.position, Quaternion.identity);
+                Debug.Log(range);
+
+                //destroying and then spawning a new note every second
+                Destroy(spawningOfNote, 1f);
+                yield return new WaitForSeconds(1f);
+            }
+            else
+            {
+                timerStatus = false;
+                Debug.Log("Notes have stopped spawning");
+            }
+        }
+
     }
 }
--- SoundMemoryMatchProto/Assets/Scripts/Timer.cs
+++ SoundMemoryMatchProto/Assets/Scripts/Timer.cs
@@ -5,6 +5,7 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     float timeLeft = 90f;
+    bool timerStatus = true;
 
 
     // Update is called once per frame
